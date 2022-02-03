9ee1e3cb26b7f72d4b1ef97d9c0ca0924bee6546
Kourtney <barnes.kourt@gmail.com>
2022-02-03 18:16:36 -0500

    push

---
+++ app.js
@@ -0,0 +1,21 @@
+function createParagraph() {
+    var word1 = document.getElementById("word1").value;
+    var word2 = document.getElementById("word2").value;
+    var word3 = document.getElementById("word3").value;
+    var word4 = document.getElementById("word4").value;
+    var word5 = document.getElementById("word5").value;
+    var word6 = document.getElementById("word6").value;
+    var word7 = document.getElementById("word7").value;
+    var word8 = document.getElementById("word8").value;
+    var word9 = document.getElementById("word9").value;
+    var word10 = document.getElementById("word10").value;
+    var word11 = document.getElementById("word11").value;
+    var word12 = document.getElementById("word12").value;
+    var word13 = document.getElementById("word13").value;
+    var word14 = document.getElementById("word14").value;
+    var word15 = document.getElementById("word15").value;
+    var word16 = document.getElementById("word16").value;
+    var word17 - document.getElementById("word17").value;
+
+    var paragraph = "When Daniel met Kamara, it was " + word1 + " at first sight. He couldn't believe his " + word2 + "! She smelled like " + word3 + " and walked as graceful as a " + word4 + ". He " + word5 + ' over to say "' + word6 + '" and introduced himself. They ' + word7 + " for a while and then exchanged " + word8 + ". On their first date, they " + word9 + " at the " + word10 + ". After a " + word11 + " night, he was " + word12 + " to bring her home. She " + word13 + 'looked at him and said "' + word14 + '" and he just knew he had to ' + word15 + ' her again. Since then, they have­­ been inseparable and he started calling her his "' + word16 + " " + word17 + '".';
+}
